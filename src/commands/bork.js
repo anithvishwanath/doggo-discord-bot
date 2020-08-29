8ac1b0763f8b8066ab447c209f07b8ade4a8cded
Anith Vishwanath <11884278+anithvishwanath@users.noreply.github.com>
2020-08-29 17:59:03 +0530

    Initial commit Initial commit for the doggo bot, consisting of the commandHandler and the commands.

---
+++ src/commands/bork.js
@@ -0,0 +1,4 @@
+module.exports = async (msg) => {
+  await msg.channel.send('bork bork!! 🐶');
+  console.log('msg sent');
+};
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+require('dotenv').config();
+
+const commandHandler = require('./commands');
+
+const client = new Discord.Client();
+
+client.once ('ready', () => {
+	console.log ('Ready!');
+});
+
+client.on('message', commandHandler);
+
+client.login(process.env.BOT_TOKEN);
